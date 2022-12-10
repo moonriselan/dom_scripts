2bff47b93aa096015bf47f33c117541aa725f6e3
Pattie Goch <patrycjagoch@gmail.com>
2022-12-10 16:02:25 +0100

    added deltions in code style to make the code more tidy

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,6 +1,7 @@
 const hamburgerRef = document.querySelector('[data-js="hamburger-btn"]');
 
-hamburgerRef.addEventListener('click', function (){
+hamburgerRef.addEventListener('click', function () {
+
     hamburgerRef.classList.toggle('drawer__hamburger--active')
     hamburgerRef.closest('[data-js="drawer"]').classList.toggle('drawer--active')
     hamburgerRef.closest('[data-js="overlay"]').classList.toggle('overlay--active')
