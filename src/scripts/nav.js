b64af2d3d0370070e0cbdc95d3301c6d3d5cc524
Pattie Goch <patrycjagoch@gmail.com>
2022-12-10 21:21:02 +0100

    cleaning code

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,7 +1,7 @@
 const hamburgerRef = document.querySelector('[data-js="hamburger-btn"]');
 const overlayRef = document.querySelector('[data-js="overlay"]');
 
-hamburgerRef.addEventListener('click', function (){
+hamburgerRef.addEventListener('click', function () {
     overlayRef.classList.toggle('active');
     console.log(overlayRef)
 });
