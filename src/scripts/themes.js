80fd38b0f49aa5aadca2a239503468316cc97056
Pattie Goch <patrycjagoch@gmail.com>
2022-12-11 11:37:27 +0100

    add js for light/dark theme

--- src/scripts/themes.js
+++ src/scripts/themes.js
@@ -1,1 +1,21 @@
+const themeSwitcherRef = document.querySelector("[data-js='theme-switch']")
 
+function readTheme(){
+const theme = localStorage.getItem('theme')
+
+    if(theme === 'dark'){
+        document.body.classList.add('dark-theme')
+        themeSwitcherRef.checked = true;
+    }
+}
+
+readTheme()
+
+themeSwitcherRef.addEventListener("change", function (){
+    document.body.classList.toggle("dark-theme")
+    if (themeSwitcherRef.checked) {
+        localStorage.setItem('theme', 'dark');
+    }else{
+        localStorage.setItem('theme', 'light');
+    }
+})
